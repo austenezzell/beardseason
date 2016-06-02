69d67772f891a0957c1102c06556abcb22d993eb
Austen <austenezzell@gmail.com>
2016-06-01 17:49:24 -0700

    g map and style

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -13,39 +13,33 @@ var aeApp = aeApp || {};
       if (currentMole == 'a'){
         console.log('a');
       }
-    })
-  }
+    });
+  };
 
   aeApp.moles = function(){
     var $molesContainer = $('.moles');
-    var numberOfMoles = 100
-
+    var numberOfMoles = 100;
+    var moleCounter = 0;
     function getRandom(min, max) {
       var roundedNumber = Math.random() * (max - min) + min;
       return Math.round(roundedNumber);
     }
-
-    var moleCounter = 0;
-
-    var growMoles = setInterval(function(){ mole() }, 3000);
-
+    var growMoles = setInterval(function(){ mole(); }, 3000);
     function mole(){
-      moleCounter ++;
       var moleSize = getRandom(6,15);
       var sideTwo = moleSize + (getRandom(-3,3));
       var badMoles = ['asymmetry', 'border', 'color', 'diameter', 'evolution'];
-
-      if (moleCounter%10 == 0) {
-        $molesContainer.append('<div class="mole bad-mole" style="height: ' + moleSize + 'px; width: ' + sideTwo + 'px; top: ' + getRandom(5,95) + '%; left: ' + getRandom(5,95) + '%; opacity: .' + getRandom(80,99) + '"><img src="/img/moles/' + badMoles[getRandom(0,4)] + '.svg"></div>')
+      moleCounter ++;
+      if (moleCounter%10 === 0) {
+        $molesContainer.append('<div class="mole bad-mole" style="height: ' + moleSize + 'px; width: ' + sideTwo + 'px; top: ' + getRandom(5,95) + '%; left: ' + getRandom(5,95) + '%; opacity: .' + getRandom(80,99) + '"><img src="/img/moles/' + badMoles[getRandom(0,4)] + '.svg"></div>');
       } else {
-        $molesContainer.append('<div class="mole ok-mole" style="height: ' + moleSize + 'px; width: ' + sideTwo + 'px; top: ' + getRandom(5,95) + '%; left: ' + getRandom(5,95) + '%; opacity: .' + getRandom(80,99) + '"></div>')
+        $molesContainer.append('<div class="mole ok-mole" style="height: ' + moleSize + 'px; width: ' + sideTwo + 'px; top: ' + getRandom(5,95) + '%; left: ' + getRandom(5,95) + '%; opacity: .' + getRandom(80,99) + '"></div>');
       }
       if (moleCounter == 200){
         stopGrowingMoles();
       }
-    };
-
-  }
+    }
+  };
 
   aeApp.ctaHover = function(){
     $('.cta').hover(function(){
@@ -70,16 +64,35 @@ var aeApp = aeApp || {};
     });
   };
 
+
+// GOOGEL MAP
+  aeApp.gmap = function() {
+
+
+  };
+
   // Define load object
   aeApp.onload = function() {
     aeApp.navigation();
   };
 
   (function($, window, document) {
-
+    aeApp.gmap();
     aeApp.onload();
     aeApp.ctaHover();
     aeApp.moles();
     aeApp.checkMoleType();
 
   }(window.jQuery, window, document));
+
+
+  var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8,
+      featureType:"all",
+      styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
+    });
+  }
+  console.log('map');
